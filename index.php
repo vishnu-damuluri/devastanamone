119da0fc040fc2cec542caa4d55d626c73dcd65a
vishnu <vishnu.damuluri@gmail.com>
2024-07-12 14:22:31 +0530

    nn

--- index.php
+++ index.php
@@ -132,7 +132,7 @@ $conn->close();
       <a href="seva.html" class="flex items-center text-md font-semibold leading-2 text-grey border-l border-r border-gray-500 px-2">Sevas</a>
       <a href="darshan.html" class="flex items-center text-md font-semibold leading-2 text-grey border-l border-r border-gray-500 px-2">Darshan</a>
         <a href="#footer"  class="flex items-center text-md font-semibold leading-2 text-grey border-l border-r border-gray-500 px-2">Contact</a>
-        <a href="https://forms.gle/iwBKHyVHWvwXgWm37" class="flex items-center text-sm font-semibold leading-2 text-grey border-l border-r border-gray-500 px-2">Donate </a>
+        <a href="https://forms.gle/iwBKHyVHWvwXgWm37" class="flex items-center text-md font-semibold leading-2 text-grey border-l border-r border-gray-500 px-2">Donate </a>
       </div>
     </nav>
     
@@ -482,7 +482,7 @@ $conn->close();
 
           </a>
           <a href="#" class="text-gray-400 hover:text-blue-4      00">
-            <span class="sr-only">X</span>
+            <span class="sr-only">Instagram</span>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4">
   <path d="M2.75 2a.75.75 0 0 0-.75.75v10.5a.75.75 0 0 0 1.5 0v-2.624l.33-.083A6.044 6.044 0 0 1 8 11c1.29.645 2.77.807 4.17.457l1.48-.37a.462.462 0 0 0 .35-.448V3.56a.438.438 0 0 0-.544-.425l-1.287.322C10.77 3.808 9.291 3.646 8 3a6.045 6.045 0 0 0-4.17-.457l-.34.085A.75.75 0 0 0 2.75 2Z" />
 </svg>
